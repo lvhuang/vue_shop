8ebe27f0969dd9658cc88a2b5028ff77f7a5bfe2
qiangyongjun918 <qiangyongjun918@163.com>
2020-03-14 23:44:27 +0800

    登录表单绘制

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,78 @@
 <template>
-    <div class="login_container">
-        <div class="login_submit"></div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 登录图标 -->
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录内容 -->
+      <div>
+        <el-form label-width="0" class="login_from">
+          <!-- 用户名 -->
+          <el-form-item>
+            <el-input></el-input>
+          </el-form-item>
+          <!-- 密码 -->
+          <el-form-item>
+            <el-input></el-input>
+          </el-form-item>
+          <!-- 按钮 -->
+          <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-.login_container{
-background-color: #2b4b6b;
-height: 100%;
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
 }
 
-.login_submit{
-width: 450px;
-height: 300px;
-background-color: #fff;
-border-radius: 3px;
-position: relative;
-left: 50%;
-top: 50%;
-transform: translate(-50%,-50%);// 固定宽高剧中
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%); // 固定宽高剧中
+
+  .avater_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 8px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_from{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
