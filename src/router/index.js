e3ccb1429af5fa5389d94dc28d390d91752b5432
qiangyongjun918 <qiangyongjun918@163.com>
2020-03-18 11:29:31 +0800

    实现首页路由重定向，路由嵌套

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
 
 Vue.use(VueRouter)
 
@@ -10,7 +11,11 @@ const router = new VueRouter({
     [
       { path: '/', redirect: '/login' },
       { path: '/login', component: Login },
-      { path: '/home', component: Home }
+      {
+        path: '/home', component: Home, redirect: '/welcome', children: [
+          { path: '/welcome', component: Welcome }
+        ]
+      }
 
     ]
 })
@@ -25,7 +30,7 @@ router.beforeEach((to, from, next) => {
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
