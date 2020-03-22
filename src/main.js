ba0a98b5579c3dfe01ec3ee9cd6851d5cfabc4d4
qiangyongjun918 <qiangyongjun918@163.com>
2020-03-22 22:21:44 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -3,25 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/gloable.css'//引入公共样式
-import axios from 'axios'// 引入axios
-
-Vue.config.productionTip = false
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'// 设置公共请求地址
-// 通过axios请求拦截器添加token验证
-axios.interceptors.request.use(config => {
-  console.log(config)
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  // 必须要return config
-  return config
-})
-// 添加响应拦截器
-axios.interceptors.response.use((response) =>{
-  // 对响应数据做点什么
-  console.log(response);
-  return response;
-});
+import axios from './utils/request'
 Vue.prototype.$http = axios// 给Vue原型加上axios
 
 
